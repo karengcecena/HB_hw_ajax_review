153aa9b89a9448daa02f725d1344813644d7be7a
karengcecena <karengcecena@gmail.com>
2022-10-13 20:21:40 -0700

    HW Complete

--- static/ajax-review.js
+++ static/ajax-review.js
@@ -2,7 +2,13 @@
 
 function showFortune() {
   // TODO: get the fortune and show it in the fortune-text div
-}
+
+  fetch("/fortune")
+    .then((response) => response.text())
+    .then((fortune) => {
+      document.querySelector("#fortune-text").innerHTML = fortune;
+    });
+};
 
 document.querySelector('#get-fortune-button').addEventListener('click', showFortune);
 
@@ -13,6 +19,11 @@ function showWeather(evt) {
   const zipcode = document.querySelector('#zipcode-field').value;
   const url = `/weather?zipcode=${zipcode}`;
   // TODO: request weather with that URL and show the forecast in #weather-info
-}
+  fetch(url)
+    .then((response) => response.json())
+    .then((serverData) => {
+      document.querySelector("#weather-info").innerHTML = serverData.forecast;
+    });
+};
 
 document.querySelector('#weather-form').addEventListener('submit', showWeather);
